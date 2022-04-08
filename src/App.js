e2e07b36ff342fde0c431e008fda033c910e3b4b
CACUCRC <carloscu@hotmail.es>
2022-04-07 23:15:14 -0600

    Generate code if url is new, retrieve if url already in db. Admin login and log out

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import './App.css';
-import { Grid, IconButton, TextField, Paper, Menu, MenuItem, Button, InputAdornment, Divider } from '@material-ui/core';
+import { Grid, IconButton, TextField, Paper, Menu, MenuItem, Button, InputAdornment, Divider, Tooltip } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import AssignmentIcon from '@material-ui/icons/Assignment';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
@@ -13,6 +13,9 @@ function App() {
   const [menu, setMenu] = useState(null)
   const [username, setUserName] = useState('')
   const [password, setPassword] = useState('')
+  const [errorLogin, setErrorLogin] = useState(false)
+  const [errorUrl, setErrorUrl] = useState(false)
+  const [errorCode, setErrorCode] = useState(false)
 
   const openMenu = (event) => {
     setMenu(event.currentTarget);
@@ -23,9 +26,15 @@ function App() {
   }
 
   const handleLogIn = () => {
-    axios.post("http://localhost:3000/login", {userName : username, password : password}).then((res) => {
-      console.log(res.status)
-      setLogged(true)
+    axios.post("http://localhost:8000/login", { userName: username, password: password }).then((res) => {
+      if (res.status === 200) {
+        setUserName('')
+        setPassword('')
+        setLogged(true)
+        setErrorLogin(false)
+      } else {
+        setErrorLogin(true)
+      }
     })
   }
 
@@ -46,33 +55,51 @@ function App() {
   }
 
   const handleGenerate = () => {
-    axios.post("http://localhost:3000/generate", {urlSent : url}).then((res) => {
-      console.log(res.status)
-    })
+    const regEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    const check = regEx.exec(url)
+    if (check) {
+      setErrorUrl(false)
+      axios.post("http://localhost:8000/generate", { urlSent: url }).then((res) => {
+        console.log(res.data)
+        if (res.status === 200) {
+          setCode(res.data.code.toString())
+        }
+      })
+    } else {
+      setErrorUrl(true)
+    }
+
   }
+
+  useEffect(() => {
+    setCode('Your code will show up here')
+  }, [])
 
   return (
     <div>
       <header className="header">
-        <Grid container direction="row" justifyContent="space-around" alignItems="center">
-          <h2>URL Shortener</h2>
-          <IconButton onClick={openMenu}>
-            <AccountCircleIcon fontSize="large" />
-          </IconButton>
+        <Grid container direction="row" justifyContent="space-between" alignItems="center">
+          <h2 className='codeShow'>URL Shortener</h2>
+          <Tooltip title="Admin options">
+            <IconButton onClick={openMenu}>
+              <AccountCircleIcon fontSize="large" className='codeCopy' />
+            </IconButton>
+          </Tooltip>
           <Menu anchorEl={menu} keepMounted open={Boolean(menu)} onClose={closeMenu}>
             {logged ?
-              <Grid container direction="column" justifyContent="space-between" alignItems="center">
-                <MenuItem>See top 20</MenuItem>
-                <MenuItem onClick={handleLogOut}>Log Out</MenuItem>
+              <Grid container direction="column" justifyContent="space-between" alignItems="center" className='loginMenu'>
+                <Button variant="contained" className='buttonsLogin' >See top 20</Button>
+                <p></p>
+                <Button variant="contained" className='buttonsLogin' onClick={handleLogOut}>Log Out</Button>
               </Grid>
-              
-            : 
-              <Grid container direction="column" justifyContent="space-between" alignItems="center">
-                <TextField label="Username" onChange={handleChangeUsername}/>
-                <TextField label="Password" onChange={handleChangePassword}/>
-                <MenuItem onClick={handleLogIn}>
+              :
+              <Grid container direction="column" justifyContent="space-between" alignItems="center" className='loginMenu'>
+                <TextField className='loginField' label="Username" onChange={handleChangeUsername} error={errorLogin} />
+                <TextField className='loginField' label="Password" onChange={handleChangePassword} error={errorLogin} />
+                <p></p>
+                <Button variant="contained" className='buttonsLogin' onClick={handleLogIn}>
                   Log In
-                </MenuItem>
+                </Button>
               </Grid>
             }
           </Menu>
@@ -80,18 +107,35 @@ function App() {
       </header>
       <body className='body'>
         <Grid container direction="column" justifyContent="space-between" alignItems="center">
-          <Grid container direction="column" justifyContent="space-around" alignItems="center">
-            <h1>Insert your URL here</h1>
-            <TextField label="URL" variant="outlined" onChange={handleChangeUrl}/>
-            <Button variant="contained" onClick={handleGenerate}>
-              Generate code
-            </Button>
-            <h2>Your code</h2>
-            <TextField placeholder="Your code will show up here" variant="outlined" InputProps={{ endAdorment : ( <InputAdornment position="end"> <AssignmentIcon/> </InputAdornment> ), }} />
-            <h1>Insert your code here</h1>
-            <TextField label="Code" variant="outlined" />
-          </Grid>
-          
+          <h1>Insert your URL here</h1>
+          <TextField label="URL" variant="outlined" className='mainField' onChange={handleChangeUrl} error={errorUrl} helperText={errorUrl ? "The URL introduced is invalid" : ""} />
+          <p></p>
+          <Button variant="contained" className='buttonsMain' onClick={handleGenerate}>
+            Generate code
+          </Button>
+          <p></p>
+          <Paper variant="outlined" elevation={0} className='code'>
+            <Grid container direction="row" justifyContent="space-between" alignItems="center">
+              <h3 className='showCode'>{code}</h3>
+              <Paper elevation={0} className='codeCopy'>
+                <Tooltip title="Copy to clipboard">
+                  <IconButton onClick={() => {
+                    if ('clipboard' in navigator) {
+                      navigator.clipboard.writeText(code)
+                    } else {
+                      document.execCommand('copy', true, code)
+                    }
+                  }}>
+                    <AssignmentIcon />
+                  </IconButton>
+                </Tooltip>
+              </Paper>
+            </Grid>
+          </Paper>
+          <h1>Insert your code here</h1>
+          <TextField className='code' label="Code" variant="outlined" />
+          <p></p>
+          <Button variant="contained" className='buttonsMain' >Go to page</Button>
         </Grid>
       </body>
     </div>
